68af40816f832c2a39fbf56dcc2bf51b10c2b8a0
AshleyDing1 <yijied@seas.upenn.edu>
2023-12-12 15:46:14 -0500

    Tasks

--- Application/src/ActuatorHandlerThread/ActuatorHandler.c
+++ Application/src/ActuatorHandlerThread/ActuatorHandler.c
@@ -1,20 +1,28 @@
 #include "ActuatorHandlerThread/ActuatorHandler.h"
 #include "SerialConsole.h"
+#include "pwmDriver/pwm.h"
 
 void vActuatorHandlerTask(void *pvParameters)
 {
 
     SerialConsoleWriteString("Get actuators functioning...\r\n");
      while(1){
-          //try to take semaphore
-          // if(xSemaphoreTake(xAccidentDetectedSemaphore, portMAX_DELAY) == pdTRUE){
-          //      SerialConsoleWriteString("Try to rescue passengers!\r\n");
-          // }
           vTaskDelay(500);
           //try to take semaphore
           if(xSemaphoreTake(xAccidentDetectedSemaphore, portMAX_DELAY) == pdTRUE){
                SerialConsoleWriteString("Try to rescue passengers!\r\n");
           }
+          configure_tcc();
+
+          //actuate servo to open the window (PA25)
+          update_pwm_duty_cycle(48000, 3); //0 position: 72000
+          
+          SerialConsoleWriteString("Window position is changed!\r\n");
+          
+          //actuate motor driver to open water pump (PA24)
+          update_pwm_duty_cycle(48000, 2);
+          
+          SerialConsoleWriteString("Water pump is opened!\r\n");
      }   
 	 
 }
