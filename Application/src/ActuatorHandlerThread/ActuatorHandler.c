dcf156b44a0a03e3c785eb98e4d78bedd84c3648
Runlong Hu <1684525025@qq.com>
2023-12-12 14:05:22 -0500

    thread communication

--- Application/src/ActuatorHandlerThread/ActuatorHandler.c
+++ Application/src/ActuatorHandlerThread/ActuatorHandler.c
@@ -7,7 +7,7 @@ void vActuatorHandlerTask(void *pvParameters)
     SerialConsoleWriteString("Get actuators functioning...\r\n");
      while(1){
           //try to take semaphore
-          if(xSemaphoreTake(xAccidentDetectedSemaphore, portMAX_DELAY);){
+          if(xSemaphoreTake(xAccidentDetectedSemaphore, portMAX_DELAY) == pdTRUE){
                SerialConsoleWriteString("Try to rescue passengers!\r\n");
           }
           vTaskDelay(500);
